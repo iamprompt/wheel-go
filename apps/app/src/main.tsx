813a7c01d5f48dfade503b2b6cf17e3a78b0f057
Supakarn Laorattanakul <wearedprompt@gmail.com>
2023-02-11 21:23:38 +0700

    tsconfig path for app

--- apps/app/src/main.tsx
+++ apps/app/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './styles/globals.css'
+import '@/styles/globals.css'
 
 const root = document.getElementById('root') as HTMLElement
 
