2967fc89f157456ce4006d30c085768e773c4e87
Supakarn Laorattanakul <wearedprompt@gmail.com>
2023-02-12 18:40:31 +0700

    add web docker images

--- apps/admin/next.config.js
+++ apps/admin/next.config.js
@@ -1,4 +1,10 @@
+const path = require('path')
+
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ['@wheel-go/ui'],
+  output: 'standalone',
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, '../..'),
+  },
 }
