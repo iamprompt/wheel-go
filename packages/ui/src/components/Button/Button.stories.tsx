1ad717b2f72f1131f4dd537198bccd5cc4b364d9
Supakarn Laorattanakul <wearedprompt@gmail.com>
2023-02-19 00:32:06 +0700

    add button variant

--- packages/ui/src/components/Button/Button.stories.tsx
+++ packages/ui/src/components/Button/Button.stories.tsx
@@ -4,9 +4,21 @@ import { Button } from './Button'
 const meta: Meta<typeof Button> = {
   title: 'Button',
   component: Button,
+  argTypes: {
+    appearance: {
+      control: {
+        type: 'select',
+        options: ['primary', 'secondary', 'dashed', 'danger'],
+      },
+    },
+  },
 }
 
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Default: Story = {}
+export const Default: Story = {
+  args: {
+    appearance: 'primary',
+  },
+}
--- packages/ui/src/components/Button/Button.tsx
+++ packages/ui/src/components/Button/Button.tsx
@@ -1,5 +1,47 @@
+import type { VariantProps } from 'class-variance-authority'
+import { cva } from 'class-variance-authority'
 import * as React from 'react'
 
-export const Button = () => {
-  return <button className="ui-bg-pink-500">Boop</button>
+const buttonVariants = cva(
+  ['ui-text-title-s', 'ui-rounded-m', 'ui-px-6', 'ui-py-3'],
+  {
+    variants: {
+      appearance: {
+        primary: ['ui-bg-magenta-500', 'ui-text-french-vanilla-100'],
+        secondary: [
+          'ui-bg-french-vanilla-100',
+          'ui-border',
+          'ui-border-french-vanilla-300',
+          'ui-text-magenta-600',
+        ],
+        dashed: ['ui-border', 'ui-border-dashed'],
+        danger: [
+          'ui-bg-french-vanilla-100',
+          'ui-border',
+          'ui-border-error-500',
+          'ui-text-error-500',
+        ],
+      },
+    },
+    defaultVariants: {
+      appearance: 'primary',
+    },
+    compoundVariants: [],
+  }
+)
+
+type ButtonProps = VariantProps<typeof buttonVariants> &
+  React.ComponentProps<'button'>
+
+export const Button: React.FC<ButtonProps> = ({ appearance, ...props }) => {
+  return (
+    <button
+      className={buttonVariants({
+        appearance,
+      })}
+      {...props}
+    >
+      Boop
+    </button>
+  )
 }
