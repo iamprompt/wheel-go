2098d4f3be76906a597320098c94fa38c2abc9c5
Supakarn Laorattanakul <wearedprompt@gmail.com>
2023-03-06 23:02:57 +0700

    add lint for admin

--- apps/admin/.eslintrc.js
+++ apps/admin/.eslintrc.js
@@ -1,4 +1,11 @@
+const path = require('path')
+
 module.exports = {
   root: true,
   extends: ['next/core-web-vitals', 'wheel-go'],
+  settings: {
+    tailwindcss: {
+      config: path.join(__dirname, 'tailwind.config.js'),
+    },
+  },
 }
