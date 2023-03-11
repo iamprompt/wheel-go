cdb6a7b82f4ce3dc94c3a938b0a9e663c8e95dce
Supakarn Laorattanakul <wearedprompt@gmail.com>
2023-03-11 16:31:01 +0700

    add records translation

--- apps/app/src/pages/record/index.tsx
+++ apps/app/src/pages/record/index.tsx
@@ -1,94 +1,75 @@
 import { Icon } from '@iconify/react'
 import { useNavigate } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
+import clsx from 'clsx'
 import { ActionTitleLayout } from '@/layouts/ActionTitle'
+
+const RecordList = [
+  {
+    title: 'favorite-places',
+    icon: 'ic:baseline-favorite-border',
+    color: 'text-pomegranate-300',
+    unit: 'places',
+    to: '/records/favorites',
+  },
+  {
+    title: 'place-reviews',
+    icon: 'ic:baseline-star-outline',
+    color: 'text-warning-400',
+    unit: 'reviews',
+    to: '/records/reviews',
+  },
+  {
+    title: 'contributed-routes',
+    icon: 'ic:baseline-route',
+    color: 'text-success-400',
+    unit: 'routes',
+    to: '/records/routes',
+  },
+]
 
 export const RecordPage = () => {
   const navigate = useNavigate()
+  const { t } = useTranslation('records')
+
   return (
     <ActionTitleLayout
       header={{
-        title: 'Records',
+        title: t('title'),
         left: false,
       }}
       bottomNav={true}
     >
-      <div className="px-4 py-6 text-title-s">Your lists</div>
+      <div className="px-4 py-6 text-title-s">{t('my-lists')}</div>
       <ul className="divide-y divide-soap-100 border border-soap-100">
-        <li
-          className="flex flex-row items-center justify-between py-3 px-4"
-          onClick={() => {
-            navigate('/records/favorites')
-          }}
-        >
-          <div className="flex flex-row items-center gap-3">
-            <Icon
-              icon="ic:baseline-favorite-border"
-              className="h-6 w-6 text-pomegranate-300"
-            />
-            <div>
-              <div className="text-title-xs">Favorite Places</div>
-              <div className="text-subtext-m text-french-vanilla-500">
-                3 places
+        {RecordList.map((record) => (
+          <li
+            key={record.title}
+            className="flex cursor-pointer flex-row items-center justify-between py-3 px-4"
+            onClick={() => {
+              navigate(record.to)
+            }}
+          >
+            <div className="flex flex-row items-center gap-3">
+              <Icon
+                icon={record.icon}
+                className={clsx('h-6 w-6', record.color)}
+              />
+              <div>
+                <div className="text-title-xs">{t(record.title)}</div>
+                <div className="text-subtext-m text-french-vanilla-500">
+                  3 {t(record.unit)}
+                </div>
               </div>
             </div>
-          </div>
-          <div>
-            <Icon
-              icon="ic:baseline-chevron-right"
-              className="h-6 w-6 text-french-vanilla-300"
-            />
-          </div>
-        </li>
-        <li
-          className="flex flex-row items-center justify-between py-3 px-4"
-          onClick={() => {
-            navigate('/records/reviews')
-          }}
-        >
-          <div className="flex flex-row items-center gap-3">
-            <Icon
-              icon="ic:baseline-star-outline"
-              className="h-6 w-6 text-warning-400"
-            />
             <div>
-              <div className="text-title-xs">Place Reviews</div>
-              <div className="text-subtext-m text-french-vanilla-500">
-                15 reviews
-              </div>
+              <Icon
+                icon="ic:baseline-chevron-right"
+                className="h-6 w-6 text-french-vanilla-300"
+              />
             </div>
-          </div>
-          <div>
-            <Icon
-              icon="ic:baseline-chevron-right"
-              className="h-6 w-6 text-french-vanilla-300"
-            />
-          </div>
-        </li>
-        <li
-          className="flex flex-row items-center justify-between py-3 px-4"
-          onClick={() => {
-            navigate('/records/routes')
-          }}
-        >
-          <div className="flex flex-row items-center gap-3">
-            <Icon
-              icon="ic:baseline-route"
-              className="h-6 w-6 text-success-400"
-            />
-            <div>
-              <div className="text-title-xs">Contributed Routes</div>
-              <div className="text-subtext-m text-french-vanilla-500">
-                20 routes
-              </div>
-            </div>
-          </div>
-          <div>
-            <Icon
-              icon="ic:baseline-chevron-right"
-              className="h-6 w-6 text-french-vanilla-300"
-            />
-          </div>
-        </li>
+          </li>
+        ))}
       </ul>
     </ActionTitleLayout>
   )
